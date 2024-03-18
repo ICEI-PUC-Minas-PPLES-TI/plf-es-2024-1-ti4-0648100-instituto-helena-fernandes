4a5aaea6a8d7e12e6cd9344cfeddeb02f8ecbcfa
Júlia Borges <juliaborgesfacul@gmail.com>
2024-03-18 15:49:25 -0300

    Configuração Spring Boot

---
+++ Codigo/backend/src/test/java/com/gerenciadorhelenafernandes/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciadorhelenafernandes.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
