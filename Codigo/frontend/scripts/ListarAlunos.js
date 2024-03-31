5b6b774fc4a601f5c38400e0c4da99ad1d1a7c44
Júlia Borges <juliaborgesfacul@gmail.com>
2024-03-31 13:24:24 -0300

    Front das telas de AceitarAluno, RecusarAluno e ListarAlunos finalizadas.

--- Codigo/frontend/scripts/ListarAlunos.js
+++ Codigo/frontend/scripts/ListarAlunos.js
@@ -1,3 +1,5 @@
+// Configura o clique no botão para mostrar/esconder os detalhes
+
 fetch('http://localhost:8080/aluno')
 .then(response => response.json())
 .then(alunos => {
@@ -15,18 +17,18 @@ fetch('http://localhost:8080/aluno')
         detalhesDiv.style.display = 'none'; // Esconde por padrão
         detalhesDiv.innerHTML = `
             
-            <p>Email: ${aluno.emailAluno}</p>
-            <p>CPF: ${aluno.cpf_aluno || 'Não informado'}</p>
-            <p>Data de Nascimento: ${aluno.data_nascimento || 'Não informado'}</p>
-            <p>Alergia: ${aluno.alergia || 'Não informado'}</p>
-            <p>Cidade: ${aluno.cidade || 'Não informado'}</p>
-            <p>Bairro: ${aluno.bairro || 'Não informado'}</p>
-            <p>Rua: ${aluno.rua || 'Não informado'}</p>
-            <p>Número da Casa: ${aluno.numero_casa || 'Não informado'}</p>
-            <p>Mais Informações: ${aluno.mais_informacoes || 'Não informado'}</p>
-            <p>Nome do Responsável: ${aluno.nome_responsavel || 'Não informado'}</p>
-            <p>CPF do Responsável: ${aluno.cpf_responsavel || 'Não informado'}</p>
-            <p>Telefone do Responsável: ${aluno.telefone_responsavel || 'Não informado'}</p>
+           <strong> <p>Email:  </strong>${aluno.emailAluno}</p>
+           <strong> <p>CPF:</strong> ${aluno.cpf_aluno || 'Não informado'}</p>
+           <strong> <p>Data de Nascimento:</strong> ${aluno.data_nascimento || 'Não informado'}</p>
+           <strong>  <p>Alergia:</strong> ${aluno.alergia || 'Não informado'}</p>
+           <strong> <p>Cidade:</strong> ${aluno.cidade || 'Não informado'}</p>
+           <strong> <p>Bairro:</strong> ${aluno.bairro || 'Não informado'}</p>
+           <strong> <p>Rua: </strong>${aluno.rua || 'Não informado'}</p>
+           <strong>  <p>Número da Casa:</strong> ${aluno.numero_casa || 'Não informado'}</p>
+           <strong> <p>Mais Informações: </strong>${aluno.mais_informacoes || 'Não informado'}</p>
+           <strong> <p>Nome do Responsável: </strong>${aluno.nome_responsavel || 'Não informado'}</p>
+           <strong> <p>CPF do Responsável:</strong> ${aluno.cpf_responsavel || 'Não informado'}</p>
+           <strong>  <p>Telefone do Responsável:</strong> ${aluno.telefone_responsavel || 'Não informado'}</p>
             <button id="botao_aprovar">Aprovar</button>
             <button id="botao_reprovar">Reprovar</button>
         `;
