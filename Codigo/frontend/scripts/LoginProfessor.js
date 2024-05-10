12227f41c99819cd9ec178b8227b2b6c83dd2003
Leticia Blom <leticiarblom@gmail.com>
2024-05-10 18:40:27 -0300

    integração back e front login professor

---
+++ Codigo/frontend/scripts/LoginProfessor.js
@@ -0,0 +1,35 @@
+function login() {
+    let emailProfessor = document.getElementById('emailProfessor').value;
+    let senhaProfessor = document.getElementById('senha_professor').value;
+
+    fetch("http://localhost:8080/professor/login", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            emailProfessor: emailProfessor,
+            senha_professor: senhaProfessor
+        })
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json(); // Retorna o corpo da resposta como JSON
+        } else {
+            throw new Error('Login inválido'); // Lança um erro para ser capturado pelo bloco catch
+        }
+    })
+    .then(data => {
+        if (data === -1) {
+            alert("Credenciais inválidas. Por favor, tente novamente.");
+        }else {
+            window.location.href = "../views/TelaProfessor.html";
+            }
+        }).catch(error => {
+        console.error('Erro ao fazer login:', error);
+        alert("Ocorreu um erro ao fazer login. Por favor, tente novamente mais tarde.");
+    });
+    
+    
+}
