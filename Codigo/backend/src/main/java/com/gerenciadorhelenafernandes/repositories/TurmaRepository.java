6b0d26d5f61e6dbe0d24bc2de09485673b1937bd
Júlia Borges <juliaborgesfacul@gmail.com>
2024-05-13 14:56:13 -0300

    Listando Turma dos Professores

--- Codigo/backend/src/main/java/com/gerenciadorhelenafernandes/repositories/TurmaRepository.java
+++ Codigo/backend/src/main/java/com/gerenciadorhelenafernandes/repositories/TurmaRepository.java
@@ -2,8 +2,10 @@ package com.gerenciadorhelenafernandes.repositories;
 
 import com.gerenciadorhelenafernandes.models.Turma;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
-@Repository
 public interface TurmaRepository extends JpaRepository<Turma, Long> {
+    @Query("SELECT t FROM Turma t JOIN t.professores p WHERE p.id_professor = :idProfessor")
+    List<Turma> findByProfessorId(Long idProfessor);
 }
